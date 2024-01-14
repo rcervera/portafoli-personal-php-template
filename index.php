9e1568e9bd54685736cb6a0f062f14c8169116c5
Ramon Cervera <rcerver4@xtec.cat>
2024-01-14 13:15:55 +0100

    primera versió

--- index.php
+++ index.php
@@ -5,10 +5,15 @@
 
 <div class="container">
  <h1>El meu portafoli d'aprenentage del llenguatge PHP</h1>
- <ul>
-    <li><a href="https://www.php.net/manual/es/getting-started.php" target="_blank" rel="noopener noreferrer">Pàgina Oficial PHP</a></li>
-    <li><a href="https://www.w3schools.com/php/default.asp" target="_blank" rel="noopener noreferrer">Curs PHP w3schools</a></li>
- </ul>
+ <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Recursos PHP</h5>
+      <ul>
+        <li><a href="https://www.php.net/manual/es/getting-started.php" target="_blank" rel="noopener noreferrer">Pàgina Oficial PHP</a></li>
+        <li><a href="https://www.w3schools.com/php/default.asp" target="_blank" rel="noopener noreferrer">Curs PHP w3schools</a></li>
+     </ul>
+    </div>
+</div>
  
 </div>
 
--- pages/basics/index.php
+++ pages/basics/index.php
@@ -5,9 +5,9 @@
 ?>
 
 <div class="container">
- <a href="variables.php">Variables</a><br>
+ 
  <a href="bucle1.php">Bucle 1</a><br>
- <a href="condicionalHora.php">Condicional Hora</a>
+ 
  
 </div>
 
